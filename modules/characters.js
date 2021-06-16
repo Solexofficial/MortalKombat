752c91254176f0cf1c6fe2e97fac3fd7eb7b0411
Mark Pokrovsky <solexofficial@yandex.ru>
2021-06-16 23:30:25 +0300

    refactor: refactoring characters create

--- modules/characters.js
+++ modules/characters.js
@@ -1,33 +1,23 @@
-import {
-  playerAttack,
-  enemyAttack,
-  changeHP,
-  elHP,
-  renderHP,
-} from './playerActions.js';
+import { playerAttack, enemyAttack } from './playerActions.js';
 
-const player1 = {
+import Player from './Player.js';
+
+const player1 = new Player({
   player: 1,
   name: 'Subzero',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
-  weapon: ['Kunai', 'Sword', 'Axe'],
+  rootSelector: 'arenas',
   attack: playerAttack,
-  changeHP,
-  elHP,
-  renderHP,
-};
+});
 
-const player2 = {
+const player2 = new Player({
   player: 2,
   name: 'Scorpion',
   hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-  weapon: ['Knife', 'Blade', 'DualSword'],
+  rootSelector: 'arenas',
   attack: enemyAttack,
-  changeHP,
-  elHP,
-  renderHP,
-};
+});
 
 export { player1, player2 };
