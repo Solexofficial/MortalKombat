6fd13bfc332f61e644592e71997e1820da490d41
Mark Pokrovsky <solexofficial@yandex.ru>
2021-06-16 23:31:36 +0300

    refactor: refact player-actions

--- modules/playerActions.js
+++ modules/playerActions.js
@@ -36,20 +36,4 @@ function enemyAttack() {
   };
 }
 
-function changeHP(damageHit) {
-  this.hp -= damageHit;
-  if (this.hp < 0) {
-    this.hp = 0;
-  }
-  return this.hp;
-}
-
-function elHP() {
-  return document.querySelector(`.player${this.player} .life`);
-}
-
-function renderHP() {
-  return (this.elHP().style.width = `${this.hp}%`);
-}
-
-export { playerAttack, enemyAttack, changeHP, elHP, renderHP };
+export { playerAttack, enemyAttack };
