b40f62a60008e16218aa1576233400c33a2198c2
Mark Pokrovsky <solexofficial@yandex.ru>
2021-06-17 19:16:40 +0300

    refactor: del comments

--- main.js
+++ main.js
@@ -1,25 +1,4 @@
-// import { player1, player2 } from './modules/characters.js';
-// import generateLogs from './modules/logs.js';
-// import { roundFight } from './modules/roundFight.js';
-// import { checkWinner } from './modules/roundEnd.js';
-// import { $formFight } from './modules/gameScene.js';
 import Game from './modules/Game.js';
-
-// function gameStart(player1, player2) {
-//   player1.createPlayer();
-//   player2.createPlayer();
-//   generateLogs('start', player1, player2);
-
-// $formFight.addEventListener('submit', function (e) {
-//   e.preventDefault();
-//   roundFight(player1, player2);
-//   checkWinner(player1, player2);
-// });
-// }
-
-// gameStart(player1, player2);
-
-// import Game from './game.js';
 
 const game = new Game();
 
