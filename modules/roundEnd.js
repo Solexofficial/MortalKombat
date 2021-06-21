2e2d6da3fc7426c02988b5b953bbdbf26d38346b
Mark Pokrovsky <solexofficial@yandex.ru>
2021-06-21 23:33:32 +0300

    refactor: refact to arrow fn

--- modules/roundEnd.js
+++ modules/roundEnd.js
@@ -2,7 +2,7 @@ import { $arenas, $formFight } from './gameScene.js';
 import { createElement } from './utils.js';
 import generateLogs from './logs.js';
 
-function checkWinner(player1, player2) {
+const checkWinner = (player1, player2) => {
   if (player1.hp <= 0 && player2.hp <= 0) {
     generateLogs('draw');
     showWinner('DRAW');
@@ -13,30 +13,30 @@ function checkWinner(player1, player2) {
     generateLogs('end', player1, player2);
     showWinner(player1.name);
   }
-}
+};
 
-function showWinner(name) {
+const showWinner = name => {
   const $winnerTitle = createElement('div', 'winnerTitle');
   $winnerTitle.innerText = name != 'DRAW' ? `${name} Wins!` : 'DRAW';
   $arenas.appendChild($winnerTitle);
   $formFight.style.display = 'none';
   showReloadButton();
-}
+};
 
-function showReloadButton() {
+const showReloadButton = () => {
   const $reloadBtn = createReloadButton();
   $reloadBtn.addEventListener('click', () => {
     window.location.pathname = 'index.html';
   });
   return $arenas.append($reloadBtn);
-}
+};
 
-function createReloadButton() {
+const createReloadButton = () => {
   const $buttonWrap = createElement('div', 'reloadWrap');
   const $button = createElement('button', 'button');
   $button.innerText = 'Restart';
   $buttonWrap.appendChild($button);
   return $buttonWrap;
-}
+};
 
 export { checkWinner, showWinner, showReloadButton, createReloadButton };
